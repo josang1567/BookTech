724fbdd88a5d92db3d893fd15257548d210247fe
AngelRabasco <rabascoangel01@gmail.com>
2021-12-03 09:18:44 +0100

    Connection created

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module controller {
-    requires javafx.controls;
-    requires javafx.fxml;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires java.persistence;
 
-    opens controller to javafx.fxml;
-    exports controller;
+	opens controller to javafx.fxml;
+
+	exports controller;
 }
